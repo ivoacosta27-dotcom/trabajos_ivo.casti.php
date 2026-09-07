a17797e6d501a10e883cf5f6990d265e84310205
ivoacosta27-dotcom <ivo.acosta27@gmail.com>
2026-09-07 09:57:34 -0300

    Add files via upload

---
+++ funciones_string.php
@@ -0,0 +1,7 @@
+<?php
+
+$cadena_texto="hola buenas";
+
+$cadena_texto=ucwords($cadena_texto);
+
+echo $cadena_texto;
---
+++ string_array.php
@@ -0,0 +1,9 @@
+<?php
+
+$fecha_1="2014/07/14";
+$fecha_2="2022/12/18";
+$numeros="Unos Dos Tres Cuatro Cinco Seis Siete";
+
+$array_numeros=explode(" ",$numeros,-2);
+
+echo $array_numeros [3];
